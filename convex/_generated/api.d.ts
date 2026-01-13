84cd7733fadda7d96c7a64daf8a1f9cbfd518758
mac-110 <alex@spschaefer.de>
2026-01-13 14:37:57 +0100

    update

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as messages from "../messages.js";
+import type * as reviews from "../reviews.js";
 
 import type {
   ApiFromModules,
@@ -18,6 +19,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   messages: typeof messages;
+  reviews: typeof reviews;
 }>;
 
 /**
